f49d239ea903323447ab17363efe6435e1d63e4b
Shivansh Mishra <156793573+beingshivansh0777@users.noreply.github.com>
2025-10-04 13:44:37 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import AddBlog from './pages/admin/AddBlog.jsx'
 import ListBlog from './pages/admin/ListBlog.jsx'
 import Comments from './pages/admin/Comments.jsx'
 import Login from './components/admin/Login.jsx'
-import Footer from './components/Footer.jsx'
+
 import 'quill/dist/quill.snow.css'
 import {Toaster} from 'react-hot-toast'
 import { useAppContext } from './context/AppContext.jsx'
@@ -32,7 +32,7 @@ const App = () => {
              <Route path='comments' element={<Comments/>}/>
         </Route>
        </Routes>
-       <Footer/>
+     
     </div>
   )
 }
